94dca88af316210feeaac1fb549e7d56c579ae41
FabioBLK <fabioblk@gmail.com>
2015-07-12 09:15:32 -0300

    Testes de roda V1

---
+++ Assets/CarControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour {
+
+	GameObject rodaFD,rodaFE,rodaTD,rodaTE;
+	//Rigidbody rbRodaFD,rbRodaFE,rbRodaTD,rbRodaTE;
+	Rigidbody rbCar;
+	public float mySpeed;
+	float currentSpeed,speed;
+	float rodaFDPosY;
+
+	// Use this for initialization
+	void Start () {
+	
+		currentSpeed = mySpeed;
+
+		rodaFD = transform.FindChild ("RodaFD").gameObject;
+		rodaFE = transform.FindChild ("RodaFE").gameObject;
+		rodaTD = transform.FindChild ("RodaTD").gameObject;
+		rodaTE = transform.FindChild ("RodaTE").gameObject;
+
+		/*
+		rbRodaFD = rodaFD.GetComponent<Rigidbody>();
+		rbRodaFE = rodaFE.GetComponent<Rigidbody>();
+		rbRodaTE = rodaTD.GetComponent<Rigidbody>();
+		rbRodaTD = rodaTE.GetComponent<Rigidbody>();
+		*/
+
+		rbCar = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		print(currentSpeed);
+		rbCar.AddForce (new Vector3(currentSpeed,0,0));
+
+
+	}
+
+	void OnCollisionEnter(Collision col){
+		currentSpeed=mySpeed;
+	}
+
+	void OnCollisionExit(Collision col){
+		//currentSpeed = 1;
+	}
+
+}
---
+++ Assets/NewRodaController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewRodaController : MonoBehaviour {
+	
+	public float maxDistance;
+	float myDistance;
+	float terrainHigh;
+	Ray ray;
+	RaycastHit hit;
+	Vector3 myPos,myParentPosition,vectorDirecao;
+	Terrain terreno;
+
+	// Use this for initialization
+	void Start () {
+		terreno = FindObjectOfType<Terrain>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myParentPosition = GetComponentInParent<Transform>().position;
+
+		myPos = transform.position;
+
+		vectorDirecao = new Vector3(0,-10,0);
+
+		terrainHigh = terreno.SampleHeight (transform.position) - 100.5f;
+
+		ray = new Ray (transform.position,vectorDirecao);
+
+		Debug.DrawRay(ray.origin,ray.direction*maxDistance);
+		print (myPos.y + " - " + maxDistance + " Terrain " + terrainHigh);
+
+		if(Physics.Raycast(ray,out hit,maxDistance)){
+				myDistance = Mathf.Lerp(myPos.y,hit.distance,0.5f)+2.0f*Time.deltaTime;
+				transform.position=new Vector3(myPos.x,myDistance,myPos.z);
+		}else{
+			myDistance = Mathf.Clamp(myParentPosition.y,myParentPosition.y-0.001f,myParentPosition.y-0.1f);
+			transform.position = new Vector3(myParentPosition.x,myDistance,myParentPosition.z);
+		}
+
+	}
+}
